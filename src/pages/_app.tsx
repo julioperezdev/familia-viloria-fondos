0ba770fc2d512eca989666b8ef93f3b138140c66
Julio Perez Viloria <protobot.dev@gmail.com>
2022-08-04 22:10:06 -0300

    adding analytics component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ENGLISH_LANGUAGE, getEnglishData, getSpanishData, SPANISH_LANGUAGE } from '../utils/database';
-import { useEffect, useState } from 'react';
-import { ParticularData } from '../models/data';
 import { LanguageProvider } from '../context/languageContext';
-import Script from 'next/script';
+import GoogleAnalytics from '@bradgarropy/next-google-analytics';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  const [language, setLanguage] = useState<ParticularData>(getSpanishData())
-  const [hasChage, setHaschage] = useState<boolean>(false)
 
   return <LanguageProvider>
-    <Script id="google-analytics" strategy="afterInteractive">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-HE2WBJVHCR');
-    `}
-    </Script>
+    <GoogleAnalytics measurementId="G-HE2WBJVHCR" />
     <Component {...pageProps} />
     </LanguageProvider>
 }
